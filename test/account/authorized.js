d0607e2a0addbead8fe8ece773aecea0f83e99de
agungandhikaf <agungandhikaf@gmail.com>
2023-02-14 15:31:27 +0700

    update authorized test

--- test/account/authorized.js
+++ test/account/authorized.js
@@ -1,10 +1,12 @@
 const chai = require('chai')
+const expect = require('chai').expect
 const chaiHttp = require('chai-http');
 const chaiJsonSchema = require('chai-json-schema');
-const expect = require('chai').expect
 chai.use(chaiHttp);
 chai.use(chaiJsonSchema);
+require('dotenv').config()
 const { ep_authorizedAccount } = require('../../src/endpoint/account/demoqa-authorized.js')
+const data = require('../../src/json-schema/authorized.json')
 const api = chai.request(process.env.baseUrl);
 
 module.exports = function(){
@@ -18,9 +20,9 @@ module.exports = function(){
                 password : process.env.password
         })
         .end(function(err, res){
-            console.log(body)
             expect(res.statusCode).to.equals(200);
-                // expect(res.body).to.be.jsonSchema(data.validData);
+            console.log(res.body)
+            expect(res.body).to.be.jsonSchema(data.validData);
             done();
         })
         })
